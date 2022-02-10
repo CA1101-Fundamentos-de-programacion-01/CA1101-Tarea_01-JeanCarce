5b1de87ac7c29fd618c3fc433067ad4c9b9f30ad
JeanCarce <98895567+JeanCarce@users.noreply.github.com>
2022-02-09 18:31:45 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -6,3 +6,11 @@ y = 3
 
 
 resultado = z + x * 365 - y * 52
+
+monedas_encontradas=20
+
+monedas_robadas=10
+
+monedas_magicas=3
+
+resultado=monedas_encontradas+monedas_robadas*365-monedas_magicas¨*52
